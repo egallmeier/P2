aaad244d341cf520ab997685bd0294f4b576710d
egallmeier <eg542@cornell.edu>
2024-10-19 13:16:53 -0700

    changes

--- vec3.hpp
+++ vec3.hpp
@@ -22,7 +22,7 @@ inline void vec3_copy(float* result, float* v)
 }
 
 inline void vec3_diff(float* result, float* a, float* b)
-{
+{ 
     result[0] = a[0]-b[0];
     result[1] = a[1]-b[1];
     result[2] = a[2]-b[2];
